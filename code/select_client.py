5a11fbb9ece4f7ed578a3f7e240e3bb85aa18ded
EdwardsTheo <baptiste.theobald@outlook.fr>
2022-03-05 09:17:04 +0100

    Ça marche askip

--- code/select_client.py
+++ code/select_client.py
@@ -5,8 +5,28 @@ import threading
 from threading import Thread
 from datetime import datetime
 from colorama import Fore, init, Back
+import random
 
 
+ # client_color = choose_color() # This is useless yes
+
+# set the available colors
+colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
+    Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
+    Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, 
+    Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
+]
+
+# choose a random color for the client It's not working UwU, Fait le marche si ça t'amuse bg 
+# client_color = random.choice(colors)
+
+nickname = input("Choisissez votre Pseudo: ")
+if nickname == 'admin':
+    password = input("Enter Password for Admin:")
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1',5002))
+
+stop_thread = False
 
 # Test connection 
 def try_connect():
@@ -18,16 +38,6 @@ def try_connect():
     except : 
         print("Le serveur ne répond pas")
         return False 
-
-def choose_nickname():
-    nickname = input("Choisissez votre Pseudo: ")
-    if nickname == 'admin':
-        password = input("Enter Password for Admin:")
-        log = [nickname, password]
-    else:
-        log = [nickname]
-    
-    return log
 
 def choose_color():
     test = True
@@ -50,23 +60,21 @@ def choose_color():
 def client_connect():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(('127.0.0.1',5002))
-    if client:
-        print("Connected")
-    return client 
 
 
-def receive(stop_thread, client, nickname, password):
+def receive():
     while True:
+        global stop_thread
         if stop_thread:
             break    
         try:
-            message = client.recv(1024).decode()
+            message = client.recv(1024).decode('utf-8')
             if message == 'NICK':
-                client.send(nickname)
-                next_message = client.recv(1024)
+                client.send(nickname.encode('utf-8'))
+                next_message = client.recv(1024).decode('utf-8')
                 if next_message == 'PASS':
-                    client.send(password)
-                    if client.recv(1024) == 'REFUSE':
+                    client.send(password.encode('utf-8'))
+                    if client.recv(1024).decode('utf-8') == 'REFUSE':
                         print("Connection is Refused !! Wrong Password")
                         stop_thread = True
                 # Clients those are banned can't reconnect
@@ -81,53 +89,29 @@ def receive(stop_thread, client, nickname, password):
             client.close()
             break
         
-def write(stop_thread, client, client_color, nickname): 
+def write(): 
     while True:
         if stop_thread:
             break
         #Getting Messages
-        date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
-        message = f'{client_color}[{date_now}]  {nickname}: {input(":")}'
+        message = f'{nickname}: {input("")}'
         if message[len(nickname)+2:].startswith('/'):
             if nickname == 'admin':
                 if message[len(nickname)+2:].startswith('/kick'):
                     # 2 for : and whitespace and 6 for /KICK_
-                    client.send(f'KICK {message[len(nickname)+2+6:]}')
+                    client.send(f'KICK {message[len(nickname)+2+6:]}'.encode('utf-8'))
                 elif message[len(nickname)+2:].startswith('/ban'):
                     # 2 for : and whitespace and 5 for /BAN
-                    client.send(f'BAN {message[len(nickname)+2+5:]}')
+                    client.send(f'BAN {message[len(nickname)+2+5:]}'.encode('utf-8'))
             else:
                 print("Commands can be executed by Admins only !!")
         else:
-            client.send(message)
+            client.send(message.encode('utf-8'))
 
-def main(): # Main program 
-    
-    connected = True
-    try:
-        client = client_connect()
-    except:
-        IndexError
-        connected = False
-    
-    if connected:
-        stop_thread = False
-        
-        log = choose_nickname()
-        log_size = len(log) 
-        nickname = log[0]
-        if log_size > 1:
-            password = log[1]
-        else:
-            password = ""
-        client_color = choose_color() # This is useless yes
-        recieve_thread = threading.Thread(target=receive, args=(stop_thread, client, client_color, nickname, password))
-        recieve_thread.start()
-        write_thread = threading.Thread(target=write, args=(stop_thread, client, nickname))
-        write_thread.start()
-    else:
-        print("error connection")
-main()
+recieve_thread = threading.Thread(target=receive)
+recieve_thread.start()
+write_thread = threading.Thread(target=write)
+write_thread.start()
 
 
     
